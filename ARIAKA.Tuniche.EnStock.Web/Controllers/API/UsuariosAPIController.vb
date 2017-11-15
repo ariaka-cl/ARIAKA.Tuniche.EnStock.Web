9ad496c58b7aa10a3167b6374dfa3abf5a7804f1
jtabilot <jtabilo@ariaka.cl>
2017-11-15 01:00:27 -0300

    Se agregan nuevos metodos para la mantencion de los usuarios.

--- ARIAKA.Tuniche.EnStock.Web/Controllers/API/UsuariosAPIController.vb
+++ ARIAKA.Tuniche.EnStock.Web/Controllers/API/UsuariosAPIController.vb
@@ -22,11 +22,20 @@ Namespace Controllers.API
             Return Me.Ok(Usuarios)
         End Function
 
-
+        <HttpDelete>
+        <Route("{id}", Name:="DeleteUser")>
+        Public Function DeleteUser(id As Integer) As IHttpActionResult
+            If id <> 0 Then
+                Return Me.Content(HttpStatusCode.OK, String.Format("Usuario Eliminado {0}", id))
+            End If
+            'TODO: Mapeo a base de datos
+            Return Me.Ok(Usuarios)
+        End Function
 
         Public Function Usuarios() As List(Of Models.UsuariosDTO)
             Dim listUser As New List(Of Models.UsuariosDTO)
-            listUser.Add(New Models.UsuariosDTO With {.Nombre = "juan", .NickName = "jbarriga", .Run = "1122222"})
+            listUser.Add(New Models.UsuariosDTO With {.ID = 1, .Nombre = "juan", .NickName = "jbarriga", .Run = "1122222"})
+            listUser.Add(New Models.UsuariosDTO With {.ID = 2, .Nombre = "pablo", .NickName = "pmarmol", .Run = "1133333"})
             Return listUser
         End Function
     End Class
--- ARIAKA.Tuniche.EnStock.Web/Scripts/app/Usuarios/index.ts
+++ ARIAKA.Tuniche.EnStock.Web/Scripts/app/Usuarios/index.ts
@@ -9,6 +9,9 @@ namespace Usuarios {
 
         public usuarios: KnockoutObservableArray<any> = ko.observableArray<any>();
         public enable: KnockoutObservable<boolean> = ko.observable(true);
+        public idRow: KnockoutObservable<number> = ko.observable(0);
+        public idRowIndex: KnockoutObservable<number> = ko.observable(-1);
+
         
         addUsuario(): void {
             let formData: App.Usuario = $('#form-user').dxForm('option').formData;
@@ -34,7 +37,22 @@ namespace Usuarios {
 
             });
         }
-        
+
+        deleteUsuario(id:number): void {
+            $.ajax({
+                type: 'DELETE',
+                url: 'api/usuarios/'+id,
+                success: (data: any): void => {
+                    //DevExpress.ui.notify("Usuario Eliminado", "success", 2000);
+                    $('#form-user').dxForm('instance').resetValues();
+                    let grid = $('#grid-user').dxDataGrid('instance');
+                    grid.refresh();
+                    grid.repaint();
+                }
+            });
+
+
+        }
         constructor() {       
 
             $.ajax({
@@ -43,6 +61,7 @@ namespace Usuarios {
                 success: (data: any): void => {
                     for (var i: number = 0; i < data.length; i++) {
                         let users = {
+                            ID: data[i].id,
                             Nombre: data[i].nombre,
                             Run: data[i].run,
                             NickName: data[i].nickName
@@ -69,15 +88,33 @@ namespace Usuarios {
 		
         dataGridOptions: any = {
 			dataSource: this.usuarios,
-            columns: ['Nombre', 'Run', 'NickName', 'Rol', 'Password'],
+            columns: [{ dataField: 'ID', visible: false }, 'Nombre', 'Run', 'NickName', 'Rol', 'Password'],
+            editing: {
+                texts: {
+                    confirmDeleteMessage: 'Esta seguro en eliminar registro?'
+                }
+            },
             onRowClick: (e) => {
                 this.enable(false);
+                let formData: any = $('#form-user').dxForm('option');
+                let userData: App.Usuario = {ID : e.data.ID,
+                                            Nombre: e.data.Nombre,
+                                            Run :e.data.Run,
+                                            NickName: e.data.NickName,
+                                            Rol: e.data.Rol,
+                                            Password: e.data.Password
+                }
+                this.idRow(userData.ID);
+                this.idRowIndex(e.rowIndex);
+                formData.formData = userData;
+                let form = $('#form-user').dxForm('instance');
+                form.repaint();
             }
 		}
 
 		buttonOptionsAdd: any = {
-			text: "Agregar",
-            icon: "plus",
+			text: "Guardar",
+            icon: "save",
             onClick: () => {
                 this.addUsuario();
             }
@@ -90,7 +127,14 @@ namespace Usuarios {
 		buttonOptionsDelete: any = {
 			text: "Borrar",
 			icon: "remove",
-			disabled: this.enable
+            disabled: this.enable,
+            onClick: () => {
+                let grid = $('#grid-user').dxDataGrid('instance');
+                let index = this.idRow();
+                grid.deleteRow(this.idRowIndex());
+                grid.repaint();
+                this.deleteUsuario(index);
+            }
 		}
 
     }
